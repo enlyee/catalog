7cd1bf4a801c4705e0a7499c12baddf689f0e63c
enlyee <enlyee@icloud.com>
2024-05-11 16:21:27 +0300

    Added Query Test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { Product } from '../src/features/products/domain/products.entity';
+import { ProductInputModel } from '../src/features/products/api/models/input/product.input.model';
 
 describe('ProductsController (e2e)', () => {
   let app: INestApplication;
@@ -50,19 +51,55 @@ describe('ProductsController (e2e)', () => {
       .expect(409);
   });
 
-  it('/ (GET) Should return created product', async () => {
+  it('/ (GET) Return all products', async () => {
+    const res = await request(app.getHttpServer()).get('/').expect(200);
+    expect(res.body).toEqual({
+      pagesCount: 1,
+      page: 1,
+      totalCount: 1,
+      items: [product],
+    });
+  });
+
+  it('/:id (GET) Should return created product', async () => {
     const res = await request(app.getHttpServer())
       .get('/' + product.id)
       .expect(200);
     expect(res.body).toEqual(product);
   });
 
-  it('/ (DELETE) Should delete created product', async () => {
+  it('/:id (DELETE) Should delete created product', async () => {
     await request(app.getHttpServer())
       .delete('/' + product.id)
       .expect(200);
     await request(app.getHttpServer())
       .get('/' + product.id)
       .expect(404);
+  });
+
+  it('/ (GET) Return all products with query', async () => {
+    const productList: ProductInputModel[] = [];
+    for (let i = 0; i < 5; i++) {
+      productList.push({
+        name: 'Plain Product' + i,
+        count: i + 1,
+        price: 1.1,
+      });
+    }
+    const res = await Promise.all(
+      productList.map(async (pr) => {
+        return request(app.getHttpServer()).post('/').send(pr);
+      }),
+    );
+    const res2 = await request(app.getHttpServer())
+      .get('/')
+      .query('sortBy=count')
+      .query('sortDirection=desc');
+    expect(res2.body).toEqual({
+      pagesCount: 1,
+      page: 1,
+      totalCount: 5,
+      items: res.map((p) => p.body).reverse(),
+    });
   });
 });
